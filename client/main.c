f2473b63bc3730cd963f152c0cde6635e766b529
Mikhail Dobryagin <dobryagin.mihail12@mail.ru>
2023-03-27 04:04:17 +0300

    Start to send requests

--- client/main.c
+++ client/main.c
@@ -15,6 +15,9 @@
 #include "req_structure.h"
 #include "transport.h"
 
+#define _g_object_type_for_transport(OBJECT_NAME) OBJECT_NAME##__t_r_a_n_s_p_o_r_t_get_type()
+#define _g_object_new(OBJECT_NAME) g_object_new(_g_object_type_for_transport(OBJECT_NAME), NULL)
+
 static DBRequestIf* client;
 
 static char* _scan_query() {
@@ -43,69 +46,67 @@ static char* _scan_query() {
 		strcpy(query_str + query_size, str_buff);
 		query_size += str_buff_len;
 	}
+	
+	return query_str;
 }
 
-int main (int argc, char** argv) {
-	while(1) {
+int main(int argc, char * argv[]) {
+	char* host = "localhost";
+	size_t port = 9090;
+
+	ThriftSocket *socket;
+	ThriftTransport *transport;
+	ThriftProtocol *protocol;
+
+	GError *error = NULL;
+
+	int exit_status = 0;
+
+#if (!GLIB_CHECK_VERSION (2, 36, 0))
+	g_type_init ();
+#endif
+
+	socket    = g_object_new (THRIFT_TYPE_SOCKET,
+							"hostname",  host,
+							"port",      port,
+							NULL);
+	transport = g_object_new (THRIFT_TYPE_BUFFERED_TRANSPORT,
+							"transport", socket,
+							NULL);
+	protocol  = g_object_new (THRIFT_TYPE_BINARY_PROTOCOL,
+							"transport", transport,
+							NULL);
+
+	thrift_transport_open (transport, &error);
+
+	client = g_object_new (TYPE_D_B_REQUEST_CLIENT,
+						 "input_protocol",  protocol,
+						 "output_protocol", protocol,
+						 NULL);
+
+	do {
 		char* query = _scan_query();
 		yy_scan_string(query);
 		struct View view;
 		yyparse(&view);
 		yylex_destroy();
-	}
-	return 0;
-}
-
-int stub_main(int argc, char * argv[])
-{
-  char* host = "localhost";
-  size_t port = 9090;
-  
-  ThriftSocket *socket;
-  ThriftTransport *transport;
-  ThriftProtocol *protocol;
-
-  GError *error = NULL;
-
-  int exit_status = 0;
-
-#if (!GLIB_CHECK_VERSION (2, 36, 0))
-  g_type_init ();
-#endif
-
-  socket    = g_object_new (THRIFT_TYPE_SOCKET,
-                            "hostname",  host,
-                            "port",      port,
-                            NULL);
-  transport = g_object_new (THRIFT_TYPE_BUFFERED_TRANSPORT,
-                            "transport", socket,
-                            NULL);
-  protocol  = g_object_new (THRIFT_TYPE_BINARY_PROTOCOL,
-                            "transport", transport,
-                            NULL);
-
-  thrift_transport_open (transport, &error);
-
-  client = g_object_new (TYPE_D_B_REQUEST_CLIENT,
-                         "input_protocol",  protocol,
-                         "output_protocol", protocol,
-                         NULL);
-
-  if (!error && d_b_request_if_ping(client, &error)) {
-    printf("Connected\n");
-  }
-  else
-  {
-    printf("Сonneсtion error\n");
-  }
+		free(query);
+		Answer_TRANSPORT* answer = g_object_new(_g_object_type_for_transport(answer), NULL);
+		GError *error = NULL;
+		printf("%d\n", d_b_request_if_do_request(client, &answer, transport_request_from_view_format(view), &error));
+		if(error)
+			printf("ERROR MSG -> %s\n", error->message);
+		else
+			printf("NO ERROR\n");
+	} while(!error && d_b_request_if_ping(client, &error));
 
 
-  thrift_transport_close (transport, NULL);
+	thrift_transport_close (transport, NULL);
 
-  g_object_unref (client);
-  g_object_unref (protocol);
-  g_object_unref (transport);
-  g_object_unref (socket);
+	g_object_unref (client);
+	g_object_unref (protocol);
+	g_object_unref (transport);
+	g_object_unref (socket);
 
-  return exit_status;
+	return exit_status;
 }
